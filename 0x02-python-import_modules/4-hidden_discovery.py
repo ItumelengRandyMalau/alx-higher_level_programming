880d3b4206c0fa2310b27242e84a04827337d4cd
ItumelengRandyMalau <itumelengRandy5@gmail.com>
2023-12-05 14:21:35 -0800

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if --name-- == "--main--":
+    import hidden_4
+    args = dir(hidden_4):
+
+        for i in range(len(args)):
+            if args[i].startswith("__")
+            continue
+        print("{}".format(arhs[i]))
