1957c25b8bbe32a866ce0e29ba73402195464b7e
ItumelengRandyMalau <itumelengRandy5@gmail.com>
2023-12-05 07:00:29 -0800

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    # Convert and sum all arguments
+    result = sum(int(arg) for arg in argv[1:])
+
+    # Print the result
+    print(result)
