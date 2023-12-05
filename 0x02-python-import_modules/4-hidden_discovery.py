145caaba4ea54cc5b2cd0c2041a349c56fd7fcb2
ItumelengRandyMalau <itumelengRandy5@gmail.com>
2023-12-05 14:46:43 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import hidden_4
 
-    module_names = dir(hidden_4)
+    mod_names = dir(hidden_4)
 
-    filtered_names = sorted(name for name in module_names if not name.startswith('__'))
+    fil_names = sorted(name for name in mod_names if not name.startswith('__'))
 
     for name in filtered_names:
         print(name)
