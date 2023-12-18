b849d0f103f7ac5dda8b1891798cf06a64ac14cd
ItumelengRandyMalau <itumelengRandy5@gmail.com>
2023-12-18 11:40:52 -0800

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    try:
+        for i in range(x):
+            # Try to print the element as an integer using "{:d}".format()
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+        print()  # Print a new line after printing all integers
+        return counter
+    except (ValueError, TypeError, IndexError):
+        print()  # Print a new line in case of an exception
+        return counter
