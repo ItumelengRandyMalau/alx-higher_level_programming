677f08ae84e863da33d7706b70094c9272c6bfc9
ItumelengRandyMalau <itumelengRandy5@gmail.com>
2023-12-18 12:19:26 -0800

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 def list_division(my_list_1, my_list_2, list_length):
     result_list = []
-    
+
     for i in range(list_length):
         try:
             # Try to perform the division element by element
             element_1 = my_list_1[i] if i < len(my_list_1) else 0
             element_2 = my_list_2[i] if i < len(my_list_2) else 0
-            
+
             # Check if both elements are numeric
             if not (isinstance(element_1, (int, float)) and isinstance(element_2, (int, float))):
                 raise TypeError("wrong type")
