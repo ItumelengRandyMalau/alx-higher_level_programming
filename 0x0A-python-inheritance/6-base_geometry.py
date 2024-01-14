a6df6a8f84fb184a28e6246ed50b58d6761db0ae
ItumelengRandyMalau <itumelengRandy5@gmail.com>
2024-01-14 10:29:14 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    # Define the class BaseGeometry
+
+    def area(self):
+        # Define a public instance method named 'area'
+        # This method raises an Exception with a specific message
+        raise Exception("area() is not implemented")
