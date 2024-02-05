e1f55d658e8d41518f5cc5983d98e5c39bdc0568
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-02-05 12:03:35 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,8 +6,4 @@
 def lookup(obj):
     """Returns a list of available attributes and
     methods of an object."""
-    return [
-        attr
-        for attr in dir(obj)
-        if not callable(getattr(obj, attr)) or not attr.startswith("__")
-    ]
+    return (dir(obj))
