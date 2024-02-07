b2bc897b452aa02f004ec0e8f8602274503ad4c5
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-02-07 06:36:59 -0800

    3-to_json_string.py completed and tested

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+""" Defines a function that returns the JSON
+    representation of an object (string):"""
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object.
+    Args:
+        my_obj(str): Data to convert to JSON.
+        Returns: JSON represantation of "my_obj"
+    """
+    return json.dumps(my_obj)
