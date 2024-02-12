7b56d30a513dd4be015ae7161a4114b0f822175b
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-02-12 05:09:54 -0800

    task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square class(child class)."""
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represents the Square class."""
+    def __init__(self, size, x=0, y=0, id=None):
+        """The Square constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Represents the  new str method for the child"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
