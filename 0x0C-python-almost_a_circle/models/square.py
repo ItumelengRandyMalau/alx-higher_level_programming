9096136209a3ad2615d4a153856af748e9260955
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-02-15 00:59:23 -0800

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,15 @@ class Square(Rectangle):
         """The Square constructor"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Represents the  new str method for the child"""
         return "[Square] ({}) {}/{} - {}".format(
