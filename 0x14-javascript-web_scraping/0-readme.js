f404c1dc345d0624d42de0499388d063783e2426
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-05-23 04:50:35 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-// Checks if the file path is provided as an argument
-if (process.argv.length !== 3) {
-  console.error('Usage: node script.js <file_path>');
-  process.exit(1);
-}
-
 // Gets the file path from the command line arguments
 const filePath = process.argv[2];
 
