e714ee2d034b690903ce2abd391d94303d083cb1
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-05-23 05:10:51 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-// Gets the file path from the command line arguments
-const file_path = process.argv[2];
-
 // Reads the content of the file
-fs.readFile(file_path, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    return;
-  }
-  console.log('File content:', data);
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
 });
