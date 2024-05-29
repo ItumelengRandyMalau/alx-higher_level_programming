4864bb9b304f63e2a7484f014b26a4943d6ffd70
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-05-29 15:20:31 -0700

    mandatory tasks

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Uses jQuery API to update the  color of div#red_header to red on click
+
+$('div#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+// updates the text of the <header> element to New Header!!! when the user cl//icks on DIV#update_header
+
+$('div#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
